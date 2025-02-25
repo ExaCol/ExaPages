0b8ada0ee7afa11f5e5e6130f2fc996405d1da39
Chakerr <luisga012@gmail.com>
2025-02-24 22:29:58 -0500

    fix: team

--- js/Scripts.js
+++ js/Scripts.js
@@ -10,3 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
       }, 500); // Espera a que termine la animación
   }, 2000); // Ajusta el tiempo de carga
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const buttons = document.querySelectorAll(".toggle-info");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", function() {
+      const card = this.parentElement;
+      card.classList.toggle("active");
+
+      // Cambiar el texto del botón según el estado
+      if (card.classList.contains("active")) {
+        this.textContent = "Menos información";
+      } else {
+        this.textContent = "Más información";
+      }
+    });
+  });
+});
